2cb85c93370d628b3459623ca996476b89cf8d64
sockbats <sackbot33@gmail.com>
2022-09-02 21:03:08 -0600

    input file for in class

--- In Class/main.cpp
+++ In Class/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include <string>
+#include <bitset>
 using namespace std;
 
 int main() {
-    string i = "hi";
-//    cin >> i;
-    getline(cin, i);
-    cout << i;
+    unsigned int num, right, left;
+    cout << "Enter the integer that holds the bits: ";
+    cin >> num;
+    cout << "\nEnter the rightmost bit position: ";
+    cin >> right;
+    cout << "\nEnter the leftmost bit position: ";
+    cin >> left;
+    bitset<32> numBin = num;
+    bitset<32> endBin(numBin.to_string().substr(31 - left, left - right + 1));
+    cout << "\nExtracting bits " << right << " through " << left <<" from " << num << " = " << numBin << ":\nResult = " << endBin.to_ulong() << " = " << endBin;
     return 0;
 }
